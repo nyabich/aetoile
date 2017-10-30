a2bbd2a0bd4c95725e1873dae1666606bd8a82bc
TECHNOLOGIE <TECHNOLOGIE@TECHNOLOGIE-PC>
2017-10-30 19:34:49 +0100

    création du projet

--- NetBeansProjects/Game/src/game/FXMLDocumentController.java
+++ NetBeansProjects/Game/src/game/FXMLDocumentController.java
@@ -10,27 +10,39 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author TECHNOLOGIE
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private Button biguinButton;
+    @FXML
+    private Text biguinLabel;
+    
+   
+    
+    private void handleButtonCommencerLaPartie(ActionEvent event) {
         System.out.println("You clicked me!");
-        System.out.println("");
-        label.setText("Hello World!");
+        
+     
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleButtonCommencerLaPartie(MouseEvent event) {
+    }
     
 }
